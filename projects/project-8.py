2e05b4b201214440a11ae32b53627dfffd7bbcbe
shams <shamsulhassan42868@gmail.com>
2026-08-16 02:16:29 -0700

    logical gates

---
+++ projects/project-8.py
@@ -0,0 +1,13 @@
+#today we will be learning the actual use of logical operators
+#Logical AND,OR and NOT gate
+# lets make the truth table of and logical gate
+print("            TRUTH TABLE OF AND GATE")
+print("                1 | 0 = ",1 and 0,"\n                1 | 1 = ",1 and 1)
+print("                0 | 0 = ",0 and 0,"\n                0 | 1 = ",0 and 1)
+print("            TRUTH TABLE OF OR GATE")
+print("                1 | 0 = ",1 or 0,"\n                1 | 1 = ",1 or 1)
+print("                0 | 0 = ",0 or 0,"\n                0 | 1 = ",0 or 1)
+print("            TRUTH TABLE OF NOT GATE")
+#this gate only inverts the origional value
+print("                1 = ",not(1))
+print("                0 = ",not(0))
